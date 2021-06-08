aecaa12635abbaaadf132bb000fb5afce84c2b2b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-06-08 15:30:16 +0200

    feat: run job on hosted runner
    BREAKING CHANGE: execute the runner via npm exec

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Firmware CI feature runner GitHub action for AWS
+# Firmware CI Device Action
 
-[![GitHub Actions](https://github.com/NordicSemiconductor/cloud-aws-firmware-ci-feature-runner-action/workflows/Test%20and%20Release/badge.svg)](https://github.com/NordicSemiconductor/cloud-aws-firmware-ci-feature-runner-action/actions)
+[![GitHub Actions](https://github.com/NordicSemiconductor/cloud-aws-firmware-ci-device-action/workflows/Test%20and%20Release/badge.svg)](https://github.com/NordicSemiconductor/cloud-aws-firmware-ci-device-action/actions)
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
 [![Renovate](https://img.shields.io/badge/renovate-enabled-brightgreen.svg)](https://renovatebot.com)
-[![Mergify Status](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/NordicSemiconductor/cloud-aws-firmware-ci-feature-runner-action)](https://mergify.io)
+[![Mergify Status](https://img.shields.io/endpoint.svg?url=https://gh.mergify.io/badges/NordicSemiconductor/cloud-aws-firmware-ci-device-action)](https://mergify.io)
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier/)
 [![ESLint: TypeScript](https://img.shields.io/badge/ESLint-TypeScript-blue.svg)](https://github.com/typescript-eslint/typescript-eslint)
 
-Uses
-[`@nordicsemiconductor/e2e-bdd-test-runner`](https://github.com/NordicSemiconductor/cloud-e2e-bdd-test-runner-js)
-to run features files of a firmware.
+GitHub action that runs firmware on a real device connected to AWS IoT via a
+self-hosted GitHub Actions runner.
+
+> :information_source:
+> [Read the complete nRF Asset Tracker documentation](https://nordicsemiconductor.github.io/asset-tracker-cloud-docs/).
---
+++ run.ts
@@ -0,0 +1,104 @@
+import { getInput, setOutput } from '@actions/core'
+import { spawn } from 'child_process'
+import * as fs from 'fs'
+
+const getRequiredInput = (input: string): string =>
+	getInput(input, { required: true }).trim()
+
+const deviceId = getRequiredInput('device id')
+const appVersion = getRequiredInput('app version')
+
+const target = getRequiredInput('target')
+const network = getRequiredInput('network')
+const secTag = parseInt(getRequiredInput('sec tag'), 10)
+const timeoutInMinutes = parseInt(getRequiredInput('timeout in minutes'), 10)
+
+const hexFile = getRequiredInput('hex file')
+const fotaFile = getRequiredInput('fota file')
+
+const multilineOrUndefined = (s: string): string[] | undefined =>
+	s.length > 0 ? s.split('\n') : undefined
+const abortOn = multilineOrUndefined(getInput('abort on'))
+const endOn = multilineOrUndefined(getInput('end on'))
+const endOnWaitSeconds = parseInt(getInput('end on waitSeconds'), 10)
+
+const certDir = getRequiredInput('certificate location')
+const flashLogLocation = getRequiredInput('flashLog output')
+const deviceLogLocation = getRequiredInput('deviceLog output')
+const jobLocation = getRequiredInput('job output')
+
+const testEnv = {
+	accessKeyId: getRequiredInput('aws access key id'),
+	secretAccessKey: getRequiredInput('aws secret access key'),
+	region: getRequiredInput('aws region'),
+	brokerHostname: getRequiredInput('broker hostname'),
+	stackName: getRequiredInput('stack name'),
+}
+
+const powerCycle = {
+	enabled: getRequiredInput('powerCycle enabled') === 'true',
+	offCmd: getRequiredInput('powerCycle offCmd'),
+	onCmd: getRequiredInput('powerCycle onCmd'),
+	waitSecondsAfterOff: parseInt(
+		getRequiredInput('powerCycle waitSecondsAfterOff'),
+		10,
+	),
+	waitSecondsAfterOn: parseInt(
+		getRequiredInput('powerCycle waitSecondsAfterOn'),
+		10,
+	),
+}
+
+const job = {
+	deviceId,
+	appVersion,
+	testEnv,
+	hexFile,
+	fotaFile,
+	abortOn,
+	endOn,
+	endOnWaitSeconds,
+	timeoutInMinutes,
+	secTag,
+	certDir,
+	target,
+	network,
+	powerCycle,
+	flashLogLocation,
+	deviceLogLocation,
+}
+
+console.log(JSON.stringify(job, null, 2))
+fs.writeFileSync(jobLocation, JSON.stringify(job, null, 2), 'utf-8')
+
+const p = spawn('npm', [
+	'exec',
+	'--',
+	'@nordicsemiconductor/firmware-ci-runner-aws',
+])
+let timedOut = false
+const t = setTimeout(() => {
+	p.kill('SIGHUP')
+	timedOut = true
+}, timeoutInMinutes * 60 * 1000)
+const data: string[] = []
+p.stdout.on('data', (d) => {
+	console.log(d.toString())
+	data.push(d.toString())
+})
+const error: string[] = []
+p.stderr.on('data', (d) => {
+	console.error(d.toString())
+	error.push(d.toString())
+})
+p.on('close', (code) => {
+	clearTimeout(t)
+	setOutput('connected', code === 0)
+	if (timedOut) {
+		console.error('Timed out.')
+		process.exit(-108)
+	}
+	process.exit(code === null ? -109 : code)
+})
+p.stdin.write(JSON.stringify(job))
+p.stdin.end()
