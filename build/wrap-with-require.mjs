a6ddffaa4f1e2fb3732d0f544393e700268ffaf9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-09-29 11:49:17 +0000

    fix: run from build folder

--- build/wrap-with-require.mjs
+++ build/wrap-with-require.mjs
@@ -1,4 +1,4 @@
 // Some dependencies use require(), so give it to them.
 import { createRequire } from 'module'
 global.require = createRequire(import.meta.url)
-import('./build/run.js')
+import('./run.js')
